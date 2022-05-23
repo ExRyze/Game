0fda39931f096a43d3ad360bd18106e1de9a0ab1
ExRyze <vaisyagovinandas@gmail.com>
2022-05-23 17:13:52 +0800

    Simplify Collision Event

--- logic.js
+++ logic.js
@@ -5,90 +5,80 @@ var obstacle = document.getElementById("obstacle");
 // User Possition Movement
 var up = 0;
 var left = 0;
-function userUp(){
+function setUserUp(){
   user.style.top = up+"px"
 };
-function userLeft() {
+function setUserLeft() {
   user.style.left = left+"px";
 }
-
-// User Weight and Height
-var userWidth = $( user ).width();
-var userHeight = $( user ).height();
-
 
 // Obstacle Possition Movement
 var stacleUp = 0;
 var stacleLeft = 400;
-function obstacleUp(){
+function setObstacleUp(){
   obstacle.style.top = stacleUp+"px"
 };
-function obstacleLeft() {
+function setObstacleLeft() {
   obstacle.style.left = stacleLeft+"px";
 }
-
-// User Weight and Height
-var obstacleWidth = $( obstacle ).width();
-var obstacleHeight = $( obstacle ).height();
 
 // Movement || Pergerakan
 // Issue {Saat menggunakan double key dan mau ke singgle key, terkadang tidak mau jalan}
 hotkeys('a,w,s,d,a+w,a+s,d+w,d+s,w+a,w+d,s+a,s+d', function (event, handler){
   switch (handler.key) {
     case 'a': left-=5;
-    userLeft(); collision();
+    setUserLeft(); collision(up, left, user, stacleUp, stacleLeft, obstacle);
       break;
     case 's': up+=5;
-    userUp(); collision();
+    setUserUp(); collision(up, left, user, stacleUp, stacleLeft, obstacle);
       break;
     case 'w': up-=5;
-    userUp(); collision();
+    setUserUp(); collision(up, left, user, stacleUp, stacleLeft, obstacle);
       break;
     case 'd': left+=5;
-    userLeft(); collision();
+    setUserLeft(); collision(up, left, user, stacleUp, stacleLeft, obstacle);
       break;
     case 'a+w': left-=5; up-=5;
-    userLeft(); userUp(); collision();
+    setUserLeft(); setUserUp(); collision(up, left, user, stacleUp, stacleLeft, obstacle);
       break;
     case 'a+s': left-=5; up+=5;
-    userLeft(); userUp(); collision();
+    setUserLeft(); setUserUp(); collision(up, left, user, stacleUp, stacleLeft, obstacle);
       break;
     case 'd+w': left+=5; up-=5;
-    userLeft(); userUp(); collision();
+    setUserLeft(); setUserUp(); collision(up, left, user, stacleUp, stacleLeft, obstacle);
       break;
     case 'd+s': left+=5; up+=5;
-    userLeft(); userUp(); collision();
+    setUserLeft(); setUserUp(); collision(up, left, user, stacleUp, stacleLeft, obstacle);
       break;
     case 'w+a': left-=5; up-=5;
-    userLeft(); userUp(); collision();
+    setUserLeft(); setUserUp(); collision(up, left, user, stacleUp, stacleLeft, obstacle);
       break;
     case 'w+d': left+=5; up-=5;
-    userLeft(); userUp(); collision();
+    setUserLeft(); setUserUp(); collision(up, left, user, stacleUp, stacleLeft, obstacle);
       break;
     case 's+a': left-=5; up+=5;
-    userLeft(); userUp(); collision();
+    setUserLeft(); setUserUp(); collision(up, left, user, stacleUp, stacleLeft, obstacle);
       break;
     case 's+d': left+=5; up+=5;
-    userLeft(); userUp(); collision();
+    setUserLeft(); setUserUp(); collision(up, left, user, stacleUp, stacleLeft, obstacle);
       break;
     default: alert(event);
   }
 });
 
-obstacleUp();
-obstacleLeft();
+setObstacleUp();
+setObstacleLeft();
 
 // Collision || Tabrakan
-function collision() {
+function collision(Uu, Ul, U, Ou, Ol, O) {
   if (
-    left + userWidth > stacleLeft &&
-    stacleLeft + obstacleWidth > left &&
-    stacleUp + obstacleHeight > up &&
-    up + userHeight > stacleUp
+    Ul + $(U).width() > Ol &&
+    Ol + $(O).width() > Ul &&
+    Ou + $(O).height() > Uu &&
+    Uu + $(U).height() > Ou
       ) {
       // collision detected!
-      console.log("obstacle");
+      alert("You die!");
   } else {
       // no collision
-      console.log(obstacleHeight);
   }};
